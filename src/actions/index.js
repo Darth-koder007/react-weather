4c6460d1e279bcbdb895717e06a1fc637ab1bcfd
Vijay Singh <vj2010sn@gmail.com>
2017-07-25 07:26:43 +0530

    component setup searchbar with input and butto

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-exports.selectBook = function (book) {
-  // selectBook is an ActionCreator, it needs to return an action,
-  // an object with a type property.
-  return {
-    type: "BOOK_SELECTED",
-    payload: book
-  };
-};
