7af8c7836b67fe567ef789699f8eb6328d8e5d07
Vijay Singh <vj2010sn@gmail.com>
2017-07-26 07:10:30 +0530

    form ready

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.state = {term: ''};
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    
+    // TODO: API fetch weather data
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
   render() {
     return(
-      <form>
-        <input />
-        <button>Submit</button>
+      <form onSubmit={this.onFormSubmit}>
+        <input
+          placeholder="Get a five day forecast for your cities"
+          value={this.state.term}
+          onInput={this.onInputChange}
+        />
+        <button type="submit">Submit</button>
       </form>
     );
   }
