aca10b80851bdbb55d477975856c270cebb737e2
Vijay Singh <vj2010sn@gmail.com>
2017-07-27 21:24:49 +0530

    http call introduced

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = `356460ab2ee307df9c0efc494b7b1b97`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = `FETCH_WEATHER`;
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
--- src/index.js
+++ src/index.js
@@ -5,22 +5,25 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 /**
  * Custom imports
  */
 import App from './components/app';
-// import reducers from "./reducers";
+import reducers from "./reducers";
 import styles from '../style/style.scss';
 import fonts from '../style/fontello.css';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 /**
  * Render function call
  */
 ReactDOM.render(
-  // <Provider store={createStore(reducers)}>
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  // </Provider>,
+  </Provider>,
     ,document.querySelector('.container')
   );
