2ae4f36746a73e3c179d0be88c5eeb296c29ea9c
Vijay Singh <vj2010sn@gmail.com>
2017-07-29 21:03:08 +0530

    add average and units

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import React, { Component } from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
-export default (props) => {
-  return (
-    <Sparklines height={120} width={180} data={props.data}>
-      <SparklinesLine color={props.color} />
-    </Sparklines>
-  );
+class Chart extends Component{
+  constructor(props) {
+    super(props);
+  }
+
+  average(data) {
+    return Math.round((data.reduce((sum, val) => sum+val, 0))/data.length);
+  }
+
+  render() {
+    return (
+      <div>
+        <Sparklines height={120} width={180} data={this.props.data}>
+          <SparklinesLine color={this.props.color} />
+          <SparklinesReferenceLine type="avg"/>
+        </Sparklines>
+        <div>
+          {this.average(this.props.data)}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Chart;
